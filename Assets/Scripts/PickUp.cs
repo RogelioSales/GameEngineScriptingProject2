518715d22bc512e29bec99af1e4ef43be9c6be3a
RogelioSales <salesrogelio.sales83@gmail.com>
2018-04-09 08:29:40 -0500

    PlayerMovement and jump pick up

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private PlayerMovement player;
+
+    private SpriteRenderer spriteRenderer;
+
+    private CircleCollider2D circleCollider2D;
+
+    // Use this for initialization
+    void Start ()
+    {     
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        circleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        { 
+            player.DoubleJumped = false;
+            spriteRenderer.enabled = false;
+            circleCollider2D.enabled = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private Transform groundCheck;
+
+    [HideInInspector]
+    public bool DoubleJumped
+    {
+        get { return doubleJumped; }
+        set { doubleJumped = value; }
+    }
+    private float groundCheckRadius = 0.2f;
+    private bool isGround = false;
+    private bool doubleJumped = true;
+    private Rigidbody2D playerrigid;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	private void Start ()
+    {
+        playerrigid = GetComponent<Rigidbody2D>();
+        doubleJumped = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void FixedUpdate ()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius,groundLayer);
+        Move();
+        Jump();
 	}
+    private void Move()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        if (movement > 0.5f || movement < -0.5f)
+        {
+            //Using the Transform component.
+            transform.Translate(new Vector2(movement * moveSpeed * Time.deltaTime, 0f));
+
+            //Using Rigidbody2D to move game Object.
+            //playerrigid.velocity = new Vector2(movement * moveSpeed * Time.deltaTime, playerrigid.velocity.y);
+        }
+    }
+    private void Jump()
+    {
+        float jumping = Input.GetAxis("Jump");
+
+        if (jumping > 0.2f && isGround)
+        {
+            isGround = false;
+            playerrigid.velocity = new Vector2(0f, jumping * jumpHeight * Time.deltaTime);
+        }
+        if (jumping >0.2f && !doubleJumped && !isGround)
+        {
+            doubleJumped = true;
+            playerrigid.velocity = new Vector2(0f,jumping*jumpHeight* Time.deltaTime);
+        }
+
+    }
 }
