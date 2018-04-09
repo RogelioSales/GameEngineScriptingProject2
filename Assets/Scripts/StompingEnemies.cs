537e7e642b21e91acfc7688b023b25e5b381d2ce
RogelioSales <salesrogelio.sales83@gmail.com>
2018-04-09 10:26:38 -0500

    enemy movemnt and killing enemy

---
+++ Assets/Scripts/StompingEnemies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompingEnemies : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask stompLayer;
+    [SerializeField]
+    private Transform stompCheck;
+
+    private float stompCheckRadius = 0.2f;
+    private bool stompable = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        stompable = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        stompable = Physics2D.OverlapCircle(stompCheck.position, stompCheckRadius, stompLayer);
+        if(stompable== true)
+        {
+            Destroy(this.gameObject);
+        }
+
+        Debug.Log(stompable);
+    }
+}
