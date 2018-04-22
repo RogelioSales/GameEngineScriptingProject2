e839403af7bb9ff4425c8729971a87949896f5cb
RogelioSales <salesrogelio.sales83@gmail.com>
2018-04-22 15:17:39 -0500

    Created the PickUp animation

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    [SerializeField]
+    private AnimationClip pickUpReturn;
     private PlayerMovement player;
     private SpriteRenderer spriteRenderer;
     private CircleCollider2D circleCollider2D;
+    private Animator anim;
+    private bool isUsed;
+    private bool isReturning;
 
     // Use this for initialization
     private void Start ()
-    {     
+    {
+        anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         circleCollider2D = GetComponent<CircleCollider2D>();
@@ -19,9 +25,12 @@ public class PickUp : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
-        { 
+        {
+            isUsed = true;
+            isReturning = false;
+            anim.SetBool("IsReturning", false);
+            anim.SetBool("IsUsed", true);
             player.DoubleJumped = false;
-            spriteRenderer.enabled = false;
             circleCollider2D.enabled = false;
             StartCoroutine(ResetPickUp());
         }
@@ -30,8 +39,13 @@ public class PickUp : MonoBehaviour
     IEnumerator ResetPickUp()
     {
         yield return new WaitForSecondsRealtime(5.0f);
-        spriteRenderer.enabled = true;
+        isUsed = false;
+        isReturning = true;
+        anim.SetBool("IsReturning",true);
+        yield return new WaitForSeconds(pickUpReturn.length);
         circleCollider2D.enabled = true;
+        anim.SetBool("IsUsed", false);
+
 
     }
 
